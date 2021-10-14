c52bd7cebd3996756b6d0d80330788b92dc06f95
2020987 <2020987@host.docker.internal>
2021-10-14 17:15:18 +0530

    Changed OrderServiceImpl, Order, OrderItem added OrderItemPk

---
+++ order-service/src/main/java/kart/shopping/orderservice/model/OrderItemPk.java
@@ -0,0 +1,86 @@
+package kart.shopping.orderservice.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+@Embeddable
+public class OrderItemPk implements Serializable{
+
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	@NotNull
+	private Order order;
+
+	@ManyToOne
+	@JoinColumn(name = "item_id")
+	@NotNull
+	private Item item;
+
+	public OrderItemPk() {
+		
+	}
+	
+	public OrderItemPk(@NotNull Order order, @NotNull Item item) {
+		super();
+		this.order = order;
+		this.item = item;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((item == null) ? 0 : item.hashCode());
+		result = prime * result + ((order == null) ? 0 : order.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItemPk other = (OrderItemPk) obj;
+		if (item == null) {
+			if (other.item != null)
+				return false;
+		} else if (!item.equals(other.item))
+			return false;
+		if (order == null) {
+			if (other.order != null)
+				return false;
+		} else if (!order.equals(other.order))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "OrderItemPk [order=" + order + ", item=" + item + "]";
+	}
+
+	
+}
