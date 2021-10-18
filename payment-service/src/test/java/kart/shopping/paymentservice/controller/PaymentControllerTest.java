de2d0573bf6c8b26fad496420b7327bac9368e54
2043823 <2043823@host.docker.internal>
2021-10-18 17:33:23 +0530

    Payment-service changes

--- payment-service/src/test/java/kart/shopping/paymentservice/controller/PaymentControllerTest.java
+++ payment-service/src/test/java/kart/shopping/paymentservice/controller/PaymentControllerTest.java
@@ -3,34 +3,36 @@ package kart.shopping.paymentservice.controller;
 import java.util.Optional;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import kart.shopping.paymentservice.entity.Address;
 import kart.shopping.paymentservice.entity.Payment;
 import kart.shopping.paymentservice.entity.PaymentType;
 import kart.shopping.paymentservice.service.PaymentService;
-import kart.shopping.paymentservice.util.EntityUtil;
 
 @SpringBootTest
 @RunWith(MockitoJUnitRunner.class)
 public class PaymentControllerTest {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(PaymentControllerTest.class);
+	
 	@InjectMocks
 	private PaymentController paymentController;
 	@Mock
 	private PaymentService paymentService;
 
 	@Test
-	public void givenString_whencreatePaymentDetails_thenReturnResponse() {
+	public void testCreatePaymentDetails() {
 		//Payment inputPayment = EntityUtil.getPayment();
-
+		LOGGER.info("testCreatePaymentDetails start");
 		Address hyd = new Address();
 		hyd.setAddressId(1);
 		hyd.setLocation("Bangalore");
@@ -45,6 +47,7 @@ public class PaymentControllerTest {
 				.thenReturn(Optional.ofNullable(payment));
 		Payment actual = paymentController.createPayment(payment);
 		Assert.assertEquals(payment, actual);
+		LOGGER.info("testCreatePaymentDetails end");
 	}
 
 }
